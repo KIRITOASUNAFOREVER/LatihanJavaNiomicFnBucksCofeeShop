210f21fd48bac18749fae66e50b01faa8040b792
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-06-30 19:16:18 +0700

    Add files via upload

---
+++ CheeseCake.java
@@ -0,0 +1,17 @@
+
+public class CheeseCake extends MenuType{
+	int getQuantity(){
+		return getMenuQuantity();
+	}
+	
+	@Override
+	int getTotalPrice() {
+		return getBasePrice() * getQuantity();
+	}
+	
+	void displayCheeseCakeOrder(){
+		super.displayCheeseCakeOrder();
+		System.out.printf("| %-20s | %-8d | %-6d | %-11d |\n",getMenuName(),getMenuQuantity(),getMenuRating(),getTotalPrice());
+		System.out.println("==========================================================");
+	}
+}
